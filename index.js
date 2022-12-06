a0e95d45aa8c122b1b6648ed66451e57fc439952
Alphalius <80674770+Alphalius@users.noreply.github.com>
2022-12-06 20:19:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let amount;
+// document.getElementById("scoreWrapper").style.display = "none"
 
 document.getElementById("calculatorButton").onclick = function() {
     
@@ -7,17 +7,17 @@ document.getElementById("calculatorButton").onclick = function() {
     userGewicht = document.getElementById("userGewicht").value;
     gedronkenGlazen = document.getElementById("gedronkenGlazen").value;
     tijdGeleden = document.getElementById("tijdGeleden").value;
-    lichaamsVocht = document.getElementById("lichaamsVocht").value;
+    // lichaamsVocht = document.getElementById("lichaamsVocht").value;
+    lichaamsVocht = document.getElementById("userSex").value
+    console.log(lichaamsVocht);
     alcoholPromillage = ((gedronkenGlazen * 10)/(userGewicht * lichaamsVocht))-(((tijdGeleden / 60) - 0.5)*(userGewicht * 0.002));
     document.getElementById("promillageParagraph").innerHTML = alcoholPromillage
 
     // Displaying the Reference Image
     if (alcoholPromillage <= 0.5 ) {
         document.getElementById("referenceImage").src="img/opgewekt.png";
-    }
-
-    // Scrolling to Part of Page
-    function scrollToSecondPart() {
-        document.getElementsByClassName("secondWrapper").scrollIntoView();
+        document.getElementById("creditsDiv").style.display = "none"
+        document.getElementById("scoreWrapper").style.display = "flex"
+        document.getElementById("scoreWrapper").style.flexDirection = "column"
     }
 }
